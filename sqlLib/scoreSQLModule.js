9c57274036cfae8da0bee6e0416eaff8cf0d9e1b
jong4876 <jong4876@users.noreply.github.com>
2018-12-28 01:12:31 +0900

    Update scoreSQLModule.js

--- sqlLib/scoreSQLModule.js
+++ sqlLib/scoreSQLModule.js
@@ -36,6 +36,25 @@ module.exports.get100Info = function(conn, ContestID, ProblemNum) { //  ContestI
 }
 
 
+module.exports.getNot100Info = function(conn, ContestID, ProblemNum) { //  ContestID, ProblemNum 백점인 사람 수
+  var sql = 'select * from student where ID in (select StudentID from score where ContestID = ? and ProblemNum = ?)';
+  var results = new Object();
+  conn.query(sql, [ContestID, ProblemNum], function(err, result, fields) {
+    if (err) {
+      console.log(err);
+      return 'Internal Server Err';
+    } else {
+      results = result;
+    }
+  });
+  while (!errorHandlingModule.isObjectData(results)) {
+    deasync.sleep(100);
+  }
+  return results;
+}
+
+
+
 module.exports.get100Count = function(conn, ContestID, ProblemNum) { //  ContestID, ProblemNum 백점인 사람 수
   var sql = 'select ProblemNum, count(*) as cnt from score group by ProblemNum';
   var results = new Object();
