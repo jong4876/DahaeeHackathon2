c586247517b57d75eb63c29b2959d7b44b19f0fb
jong4876 <jong4876@users.noreply.github.com>
2018-12-27 10:43:02 +0900

    Update studentSQLModule.js

--- sqlLib/studentSQLModule.js
+++ sqlLib/studentSQLModule.js
@@ -3,14 +3,33 @@ var deasync = require('deasync');
 var errorHandlingModule = require('../errorHandlingModule.js');
 
 var conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: 'www5.dynu.net',
+  port: '43306',
+  user: 'myadmin',
   //user: 'debian-sys-maint',   //AWS 전용
-  password: '111111',
+  password: 'myadmin',
   //password: 'HoF7vJTdGAyfMvIc',
   database: 'hackathondb'
 });
 conn.connect();
+
+module.exports.getInfo = function(ID) {
+  var sql = 'select * from student';
+  var results = new Object();
+  conn.query(sql, [ID], function(err, result, fields) {
+    if (err) {
+      console.log(err);
+      return 'Internal Server Err';
+    } else {
+      results = result;
+    }
+  });
+  while (!errorHandlingModule.isObjectData(results)) { // 비동기 처리
+    deasync.sleep(100);
+  }
+
+  return results;
+}
 
 module.exports.getInfoByID = function(ID) {
   var sql = 'select * from student where ID = ? ';
