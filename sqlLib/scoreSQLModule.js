99a63141bc7d9405200591c44767df2844203663
rkddlsgur983 <rkddlsgur983@gmail.com>
2018-12-28 01:17:35 +0900

    Update scoreSQLModule.js

--- sqlLib/scoreSQLModule.js
+++ sqlLib/scoreSQLModule.js
@@ -35,6 +35,22 @@ module.exports.get100Info = function(conn, ContestID, ProblemNum) { //  ContestI
   return results;
 }
 
+module.exports.getNot100Info = function(conn, ContestID, ProblemNum) { //  ContestID, ProblemNum 백점인 사람 수
+  var sql = 'select * from student where ID in (select StudentID from score where ContestID = ? and ProblemNum = ? and Score != 100)';
+  var results = new Object();
+  conn.query(sql, [ContestID, ProblemNum], function(err, result, fields) {
+    if (err) {
+      console.log(err);
+      return 'Internal Server Err';
+    } else {
+      results = result;
+    }
+  });
+  while (!errorHandlingModule.isObjectData(results)) {
+    deasync.sleep(100);
+  }
+  return results;
+}
 
 module.exports.get100Count = function(conn, ContestID, ProblemNum) { //  ContestID, ProblemNum 백점인 사람 수
   var sql = 'select ProblemNum, count(*) as count from score group by ProblemNum';
