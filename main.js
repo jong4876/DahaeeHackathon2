511a22f3aa2da780c0850245042274e0009ecfa2
jong4876 <jong4876@users.noreply.github.com>
2018-12-27 19:26:10 +0900

    all 복귀

--- main.js
+++ main.js
@@ -32,8 +32,6 @@ app.get('/test', function(req, res) {
   var studentAVGInfo = studentSQLModule.getAVGInfo(conn);
   var studentInfoUp20per = studentSQLModule.getInfoUp20per(conn);
 
-  var student
-
   var scoreNot100Info = scoreSQLModule.getNot100Info(conn,1,1); // 100점 사람  4번
   var score100Info = scoreSQLModule.get100Info(conn,1,1);
 
--- sqlLib/studentSQLModule.js
+++ sqlLib/studentSQLModule.js
@@ -2,7 +2,23 @@
 var deasync = require('deasync');
 var errorHandlingModule = require('../errorHandlingModule.js');
 
-
+module.exports.getInfo = function(conn) { // 2번
+  var sql = 'select * from student order by ID';
+  var jsonTotalArray = new Array();
+  var results = new Object();
+  conn.query(sql, function(err, result, fields) {
+    if (err) {
+      console.log(err);
+      return 'Internal Server Err';
+    } else {
+      results = result;
+    }
+  });
+  while (!errorHandlingModule.isObjectData(results)) { // 비동기 처리
+    deasync.sleep(100);
+  }
+  return results;
+}
 module.exports.getSW18Info = function(conn) { // 2번
   var sql = 'select * from student order by SWContest18 desc';
   var jsonTotalArray = new Array();
